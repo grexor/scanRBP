92ef0e01a26f5ef0b3489124edfaccdc2006a7f2
Gregor Rot <gregor.rot@gmail.com>
2023-11-09 15:44:43 +0100

    Docs

--- scanRBP/database/__init__.py
+++ scanRBP/database/__init__.py
@@ -6,12 +6,12 @@ import sys
 proteins = {}
 
 def init():
-    download = False
     database_file = f"{scanRBP.config.data_folder}/scanRBP.tab.gz"
     if not os.path.exists(database_file):
         if not os.path.exists(scanRBP.config.data_folder):
             os.makedirs(scanRBP.config.data_folder)
-        os.system(f"wget https://github.com/grexor/scanRBP/raw/main/data/scanRBP.tab.gz -O {scanRBP.config.data_folder}/scanRBP.tab.gz >/dev/null 2>&1")
+        os.system(f"wget https://github.com/grexor/scanRBP/raw/main/data/data.tar.gz -O {scanRBP.config.data_folder}/data.tar.gz >/dev/null 2>&1")
+        os.system(f"tar xfz {scanRBP.config.data_folder}/data.tar.gz -C {scanRBP.config.data_folder}")
     f = gzip.open(database_file, "rt")
     header = f.readline().replace("\r", "").replace("\n", "").split("\t")
     r = f.readline()
